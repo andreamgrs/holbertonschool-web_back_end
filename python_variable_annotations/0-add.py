369ad63280f594e3b0b358a4b431598b67eaffb8
andreamgrs <andrea.munozgr@gmail.com>
2025-11-24 17:00:25 +1100

    Adding task 1

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+""" Module for function that takes two float
+arguments and returns the sum as float """
+
+
 def add(a: float, b: float) -> float:
+    """Returns the sum of both arguments"""
     return a + b

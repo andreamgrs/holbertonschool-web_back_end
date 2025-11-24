f7cc748e853aed3eac5a06c64fef85309f8d5816
andreamgrs <andrea.munozgr@gmail.com>
2025-11-24 17:47:04 +1100

    Adding task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that define and annotate variables
+with the specified values """
+
+a: str = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"

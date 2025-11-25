97eee534530b47f47f9f66ae7e25b67f562768e4
andreamgrs <andrea.munozgr@gmail.com>
2025-11-25 22:44:56 +1100

    Adding task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Module for function make_multiplier that takes a float
+multiplier as argument and returns a function that multiplies
+a float by multiplier. """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    # Callable: Returns a func that takes a float and returns float.
+    def function_to_return(var: float) -> float:
+        return var * multiplier
+    return function_to_return

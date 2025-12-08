681d8d1121a4ef6b994572e3bdbc838f5b17c005
andreamgrs <andrea.munozgr@gmail.com>
2025-12-08 19:05:54 +1100

    Adding task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,4 +11,4 @@ async def async_generator() -> Generator[float, None, None]:  # type: ignore
     wait 1 second, then yield a random number between 0 and 10. """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)

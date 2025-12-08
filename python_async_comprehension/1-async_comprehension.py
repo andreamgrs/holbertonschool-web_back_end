66131f79fd7d526e0d1f2726aba66c159911399b
andreamgrs <andrea.munozgr@gmail.com>
2025-12-08 20:02:24 +1100

    Adding task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """ Module for coroutine called async_comprehension
 that takes no arguments.. """
 import asyncio
-import random
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():  # type: ignore
+async def async_comprehension() -> List[float]:  # type: ignore
     """ Coroutine will collect 10 random numbers using an async
     comprehensing over async_generator, then return the 10 random numbers. """
     result = [x async for x in async_generator()]

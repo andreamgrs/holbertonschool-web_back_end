7b04b5a33d8f4a5f6bf08e10f3bb77daa65da948
andreamgrs <andrea.munozgr@gmail.com>
2025-12-08 22:30:15 +1100

    Adding task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,7 +3,7 @@
 four times in parallel using asyncio.gather. """
 import asyncio
 import time
-async_comprehension = __import__('1-async_comprehension').async_generator
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:

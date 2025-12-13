40d4625cc300fb68741e662e3d3d732f46d1772c
andreamgrs <andrea.munozgr@gmail.com>
2025-12-13 16:40:22 +1100

    adding task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -27,8 +27,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert page > 0
-            assert page_size > 0
+            assert isinstance(page, int) and page > 0
+            assert isinstance(page_size, int) and page_size > 0
             database = self.dataset()
             start, end = index_range(page, page_size)
 

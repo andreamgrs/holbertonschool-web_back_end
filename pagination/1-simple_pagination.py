aca23353b2a277b0670557456cbb7b97bc89a312
andreamgrs <andrea.munozgr@gmail.com>
2026-01-03 22:08:22 +1100

    Adding 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -27,14 +27,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
-            database = self.dataset()
-            start, end = index_range(page, page_size)
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        database = self.dataset()
+        start, end = index_range(page, page_size)
 
-            if start >= len(database):
-                []
-            return database[start:end]
+        if start >= len(database):
+            []
+        return database[start:end]
 
 
 def index_range(page: int, page_size: int) -> Tuple:

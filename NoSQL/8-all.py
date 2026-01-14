991cf47b8b9972e1db39a11fa81063a4af629192
andreamgrs <andrea.munozgr@gmail.com>
2026-01-14 13:07:16 +1100

    Adding task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+"8-all.py" module
+"""
+
+
+def list_all(mongo_collection):
+    """
+    function that lists all documents in a collection
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())

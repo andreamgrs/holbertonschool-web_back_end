8c11705cd14e984bee4f701404a8ccafa07ae6a4
andreamgrs <andrea.munozgr@gmail.com>
2026-01-30 22:35:19 +1100

    Adding task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,28 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square feet must be a number');
+    }
+    
+    this._sqft = sqft;
+
+    if (new.target !== Building) {
+      throw new Error('Building is an abstract class');
+      if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+
+  }
+
+  get sqft() { return this._sqft; }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+}

876cabe00f23a2d37adc4f6f4cc1586f7d43cd75
andreamgrs <andrea.munozgr@gmail.com>
2026-01-30 22:01:14 +1100

    Adding task 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,5 +1,13 @@
 export default class Currency {
   constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+
     this._code = code
     this._name = name
   }
@@ -8,7 +16,7 @@ export default class Currency {
     return this._code
   }
   set (newCode) {
-    return this._name = newCode
+    return this._code = newCode
   }
   get (name) {
     return this._name

fd210abb8afdc51ef7efc300f65127a3e34e5660
andreamgrs <andrea.munozgr@gmail.com>
2026-01-30 23:11:05 +1100

    Adding task 8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+  // Cast to Number
+  valueOf() {
+    return this._size;
+  }
+
+  // Cast to String
+  toString() {
+    return this._location;
+  }
+
+}

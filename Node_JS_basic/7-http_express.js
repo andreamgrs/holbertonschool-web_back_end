eecd1da069786073e5d59df1717e0bae0d254dfd
andreamgrs <andrea.munozgr@gmail.com>
2026-04-15 15:17:39 +1000

    Completed task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const fs = require('fs');
+
+const arg = process.argv[2];
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
+  fs.readFile(arg, 'utf8', (err, data) => {
+    if (err) {
+      res.end('Cannot load the database');
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const students = lines.slice(1);
+
+    res.write(`Number of students: ${students.length}\n`);
+
+    const fields = {};
+
+    students.forEach((line) => {
+      const parts = line.split(',');
+      const firstname = parts[0];
+      const field = parts[3];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    for (const field in fields) {
+      if (fields[field]) {
+        res.write(
+          `Number of students in ${field}: ${
+            fields[field].length
+          }. List: ${fields[field].join(', ')}\n`,
+        );
+      }
+    }
+
+    res.end();
+  });
+});
+
+app.listen(1245, () => {
+  console.log('Server running on port 1245');
+});
+
+module.exports = app;

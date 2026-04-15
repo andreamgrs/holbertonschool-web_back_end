cad9f8cc3ab157e4d7f8863ae48e3fbc23bad97b
andreamgrs <andrea.munozgr@gmail.com>
2026-04-15 13:59:09 +1000

    Completed task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding("utf8");
+process.stdin.setEncoding('utf8');
 
-process.stdin.on("data", (data) => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
